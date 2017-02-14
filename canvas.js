032b56a8be159bd39afd67fbe92809b0c2971c86
Xinhui <x.xu.1102@gmail.com>
2017-02-14 14:52:35 -0500

    0214

--- canvas.js
+++ canvas.js
@@ -8,12 +8,22 @@ clearbtn.addEventListener("click", function(e){
     alert("Your canvas has been magically cleared!");
 });
 
-canvas.addEventListener("click", function(e){
+var clickrect = function(e) {
     var ctx = canvas.getContext('2d');
     var stroke = 10;
     var rect = canvas.getBoundingClientRect();
     var xcor = event.clientX - rect.left - 0.5*stroke;
     var ycor = event.clientY - rect.top - 0.5*stroke;
     ctx.fillRect(xcor, ycor, stroke, stroke);
+};
+var ctx = canvas.getContext('2d');
+ctx.beginPath();
+canvas.addEventListener("click", function(e){
+    var stroke = 10;
+    var rect = canvas.getBoundingClientRect();
+    var xcor = event.clientX - rect.left - 0.5*stroke;
+    var ycor = event.clientY - rect.top - 0.5*stroke;
+    ctx.arc(xcor, ycor, stroke*0.5, 0, 2*Math.PI);
+    ctx.stroke();
 });
 
