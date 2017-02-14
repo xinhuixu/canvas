577a639bbe6ccfb12b382ee5b21cea8992ae65f7
Brian-Lu <brianlu22@yahoo.com>
2017-02-13 21:43:40 -0500

    FillRect

--- canvas.js
+++ canvas.js
@@ -5,3 +5,10 @@ button.addEventListener("click", function(e){
     var ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
 });
+
+canvas.addEventListener("click", function(e){
+    var ctx = canvas.getContent('2d');
+    var xcor = event.clientX;
+    var ycor = event.clientY;
+    ctx.fillRect(xcor, ycor, 200, 200);
+});
